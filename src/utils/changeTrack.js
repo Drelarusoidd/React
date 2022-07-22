b220749bc0b3e0d660dfa0f4720d68db705e3d36
Drelarusoid <buble.1205@gmail.com>
2022-07-22 11:59:22 +0300

    update changeTrack.js

--- src/utils/changeTrack.js
+++ src/utils/changeTrack.js
@@ -1,28 +1,23 @@
 function changeTrack(method, data, currentUrl) {
     const trackSet = data['track_set'];
-    const currentIndex = trackSet.findIndex(value => value['track'] === currentUrl)
+    const currentIndex = trackSet.findIndex(value => value['track'] === currentUrl);
 
+    let targetIndex = 0;
     if (method === 'next') {
-        if (currentIndex + 1 === trackSet.length) {
-          var url = trackSet[0]['track'];
-          var cover = trackSet[0]['cover'];
-          var trackName = trackSet[0]['name'];
-        }else {
-          var url = trackSet[currentIndex + 1]['track'];
-          var cover = trackSet[currentIndex + 1]['cover'];
-          var trackName = trackSet[currentIndex + 1]['name'];
+        if (currentIndex + 1 !== trackSet.length) {
+          targetIndex = currentIndex + 1;
         }
     }else {
         if (currentIndex - 1 === -1) {
-          var url = trackSet[trackSet.length - 1]['track']
-          var cover = trackSet[trackSet.length - 1]['cover'];
-          var trackName = trackSet[trackSet.length - 1]['name'];
+          targetIndex = trackSet.length - 1;
         }else {
-          var url = trackSet[currentIndex - 1]['track'];
-          var cover = trackSet[currentIndex - 1]['cover'];
-          var trackName = trackSet[currentIndex - 1]['name'];
+          targetIndex = currentIndex - 1;
         }
     }
+    
+    const url = trackSet[targetIndex]['track'];
+    const cover = trackSet[targetIndex]['cover'];
+    const trackName = trackSet[targetIndex]['name'];
     return [url, cover, trackName];
 }
 
